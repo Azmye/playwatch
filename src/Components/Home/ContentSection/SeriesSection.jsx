cc670508f2a6f6f0b2ef1faaa9074e5eb4af5196
azmye <mhmmdazmye@gmail.com>
2024-07-22 20:50:20 +0700

    fix: blank page

--- src/Components/Home/ContentSection/SeriesSection.jsx
+++ src/Components/Home/ContentSection/SeriesSection.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import useFetch from '../../../Hooks/useFetch';
-import ContentLoading from '../../ContentLoader/ContentLoading';
-import SlideContent from '../../ContentLoader/SlideContent';
-import SeriesSlider from '../../Series/SeriesSlider';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import useFetch from "../../../Hooks/useFetch";
+import ContentLoading from "../../ContentLoader/ContentLoading";
+import SlideContent from "../../ContentLoader/SlideContent";
+import SeriesSlider from "../../Series/SeriesSlider";
 
 const SeriesSection = () => {
-  const { data: series, isLoading, error } = useFetch('/discover/tv', 'popularity.desc');
+  const { data: series, isLoading, error } = useFetch("/discover/tv");
   const scrollTo = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
   return (
     <section id="tv">
@@ -16,7 +16,7 @@ const SeriesSection = () => {
         <div className="pb-5 flex justify-between text-white">
           <h2 className="font-semibold text-2xl">TV Shows</h2>
           <nav>
-            <Link onClick={scrollTo} to={'/tv'}>
+            <Link onClick={scrollTo} to={"/tv"}>
               View All
             </Link>
           </nav>
--- src/Components/Movies/MovieGrid.jsx
+++ src/Components/Movies/MovieGrid.jsx
@@ -1,11 +1,13 @@
-import useFetch from '../../Hooks/useFetch';
-import ContentLoading from '../ContentLoader/ContentLoading';
-import MovieItem from './MovieItem';
+import useFetch from "../../Hooks/useFetch";
+import ContentLoading from "../ContentLoader/ContentLoading";
+import MovieItem from "./MovieItem";
 
 const MovieGrid = () => {
-  const { data: movies, isLoading, error } = useFetch('/discover/movie', 'popularity.desc');
+  const { data: movies, isLoading, error } = useFetch("/discover/movie");
   return (
-    <div className={`grid grid-cols-3 gap-3 lg:gap-5 text-white lg:grid-cols-5`}>
+    <div
+      className={`grid grid-cols-3 gap-3 lg:gap-5 text-white lg:grid-cols-5`}
+    >
       {error && <div>{error}</div>}
       {isLoading && <ContentLoading count={20} />}
       {movies && <MovieItem movies={movies} />}
